39ad9e0d6be14edb5eb80746ede4f1bc15ff6cfe
Tom Groombridge <groombridgetom@gmail.com>
2017-09-19 21:16:12 +0100

    changed naming convention

--- lib/command.rb
+++ lib/command.rb
@@ -2,14 +2,13 @@ class Command
 
   def initialize(bitmap, line)
     @bitmap = bitmap
-    @line = line
-    @chars = @line.split(' ')
-    @colour = @chars.last
+    @line = line.split(' ')
+    @colour = @line.last
   end
 
   def create_bitmap
-    num_of_rows = @chars[2].to_i
-    num_of_collums = @chars[1].to_i
+    num_of_rows = @line[2].to_i
+    num_of_collums = @line[1].to_i
     num_of_rows.times do
       @bitmap << Array.new(num_of_collums, '0')
     end
@@ -17,22 +16,22 @@ class Command
   end
 
   def change_pixel_colour
-    num_of_collums = string_to_index(@chars[1])
-    num_of_rows = string_to_index(@chars[2])
+    num_of_collums = string_to_index(@line[1])
+    num_of_rows = string_to_index(@line[2])
     @bitmap[num_of_rows][num_of_collums] = @colour
     @bitmap
   end
 
   def add_vertical_line_color
-    line_length(@chars[2], @chars[3])
-    collum = string_to_index(@chars[1])
+    line_length(@line[2], @line[3])
+    collum = string_to_index(@line[1])
     @bitmap[@line_start..@line_end].each {|i| i[collum] = @colour}
     @bitmap
   end
 
   def add_horizontal_line
-    line_length(@chars[1], @chars[2])
-    row = string_to_index(@chars[3])
+    line_length(@line[1], @line[2])
+    row = string_to_index(@line[3])
     (@line_start..@line_end).each {|num| @bitmap[row][num] = @colour}
     @bitmap
   end
